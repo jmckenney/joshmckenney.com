3f3dc4dc39f1997be5e052dd6d29c29221e4dba5
jmckenney <josh.mckenney@swift.co>
2017-05-15 20:58:14 -0700

    Webkit watch in place and dev server auto reloading wonderfully

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 The site, it all it's sheer glory.
 
 #To run live dev environment:
-$ node_modules/.bin/webpack-dev-server
+$ yarn run build
+$ yarn run start
 
 #To deploy to google cloud app engine:
 gcloud app deploy app.yaml --project [projectname]
--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
+// page load intro
 var Vivus = require('../node_modules/vivus');
 require('./header.scss');
 
-console.log('starting');
+var logoElement = document.getElementById("svg-logo");
+
 new Vivus('svg-logo', {duration: 200, animTimingFunction: Vivus.EASE}, setupNav);
 
 function setupNav() {
-    console.log('moving logo to menu');
+    logoElement.setAttribute("class", "svg-logo small");
 }
